91db897a0f6b025be6b61440bba62c6517934e58
Joeffrey <brionesjoeffrey@gmail.com>
2025-11-28 18:57:45 -0500

    User-defined exceptions

---
+++ Exceptions/MemberAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class MemberAlreadyExistsException extends Exception {
+    public MemberAlreadyExistsException(String msg) {
+        super(msg);
+    }
+}
---
+++ Exceptions/MovieUnavailableException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class MovieUnavailableException extends Exception {
+    public MovieUnavailableException(String msg) {
+        super(msg);
+    }
+}
