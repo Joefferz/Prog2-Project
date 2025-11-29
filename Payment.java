3dc9d8c9d6c32f4cd6551138f38cc437ccd36d5e
Joeffrey <brionesjoeffrey@gmail.com>
2025-11-29 12:29:28 -0500

    Payment javadoc

--- Payment.java
+++ Payment.java
@@ -1,6 +1,20 @@
+/**
+ * Represents a payment calculation system.
+ * Implementing classes must define how the rental fee is calculated based on a customer's membership type.
+ */
 interface Payment {
+    /** The rental fee charged to student members. */
     public final double STUDENT_FEE = 5;
+
+    /** The rental fee charged to external members. */
     public final double EXTERNAL_MEMBER_FEE = 10;
 
+    /**
+     * Calculates the rental fee according to the given membership type.
+     * Classes that implements this class must provide their own implementation of this method (override).
+     *
+     * @param membership the type of membership (Student/External)
+     * @return the rental fee for that membership type
+     */
     public abstract double calculate(String membership);
 }
