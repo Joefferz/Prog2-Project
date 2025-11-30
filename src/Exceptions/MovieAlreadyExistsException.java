b459a77238ece3b155d8d003a1eaccf5ab8b7412
Joefferz <brionesjoeffrey@gmail.com>
2025-11-29 19:20:04 -0500

    Update MovieAlreadyExistsException.java

--- src/Exceptions/MovieAlreadyExistsException.java
+++ src/Exceptions/MovieAlreadyExistsException.java
@@ -1,7 +1,18 @@
 package Exceptions;
 
+/**
+ * Exception thrown when attempting to add a movie
+ * whose ID already exists in the system.
+ */
 public class MovieAlreadyExistsException extends Exception {
+
+    /**
+     * Constructs a new MovieAlreadyExistsException with the specified detail message.
+     *
+     * @param message the detail message explaining the exception
+     */
     public MovieAlreadyExistsException(String msg) {
         super(msg);
     }
+
 }
