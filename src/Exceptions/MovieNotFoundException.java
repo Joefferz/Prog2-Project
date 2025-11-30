35505d6c1c7e67acef82212c174376b916f967dd
Joefferz <brionesjoeffrey@gmail.com>
2025-11-29 19:20:49 -0500

    Update MovieNotFoundException.java

--- src/Exceptions/MovieNotFoundException.java
+++ src/Exceptions/MovieNotFoundException.java
@@ -1,7 +1,18 @@
 package Exceptions;
 
+/**
+ * Exception thrown when attempting to rent a movie
+ * that is currently unavailable (already rented out).
+ */
 public class MovieNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs a new MovieUnavailableException with the specified detail message.
+     *
+     * @param message the detail message explaining the exception
+     */
     public MovieNotFoundException(String msg) {
         super(msg);
     }
 }
+
