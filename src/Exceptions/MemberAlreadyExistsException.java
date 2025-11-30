9c60e80002917e197cacc36342e6b4c6afedc12c
Joefferz <brionesjoeffrey@gmail.com>
2025-11-29 19:19:11 -0500

    Update MemberAlreadyExistsException.java

--- src/Exceptions/MemberAlreadyExistsException.java
+++ src/Exceptions/MemberAlreadyExistsException.java
@@ -1,7 +1,18 @@
 package Exceptions;
 
+/**
+ * Exception thrown when attempting to add a member
+ * whose customer ID already exists in the system.
+ */
 public class MemberAlreadyExistsException extends Exception {
+
+    /**
+     * Constructs a new MemberAlreadyExistsException with the specified detail message.
+     *
+     * @param message the detail message explaining the exception
+     */
     public MemberAlreadyExistsException(String msg) {
         super(msg);
     }
+
 }
